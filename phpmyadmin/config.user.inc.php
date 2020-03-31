466afff8acc84151697a25bea8c747a92c432901
Jörn Velten <joernvigb@aol.com>
2020-03-31 13:44:24 +0200

    feat: added ftp-server feat: added config for phpmyadmin

---
+++ phpmyadmin/config.user.inc.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @copyright (c) 2020 wissen.online
+ * @author        Jörn Velten, pronego (https://www.pronego.com)
+ * @date          2020-03-16 8:34
+ */
+
+$cfg['MaxNavigationItems'] = 300;
+$cfg['LoginCookieValidity'] = 604800; // 7 days
